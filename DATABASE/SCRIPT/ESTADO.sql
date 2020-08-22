85fec19ef4bceb574b2e683294d425e6f3e5cc6c
Edgar Rangel - Neoris <e-edgarrangel@neoris.com>
2020-08-21 19:03:00 -0500

    scripts para poblar la base de datos terminado

---
+++ DATABASE/SCRIPT/ESTADO.sql
@@ -0,0 +1,37 @@
+--------------------------------------------------------
+-- Archivo creado  - viernes-agosto-21-2020   
+--------------------------------------------------------
+REM INSERTING into ALEJANDRO.ESTADO
+SET DEFINE OFF;
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (1,'Aguascalientes','AGU');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (2,'Baja California','BCN');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (3,'Baja California Sur','BCS');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (4,'Campeche','CAM');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (5,'Chiapas','CHP');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (6,'Chihuahua','CHH');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (7,'Ciudad de MÃ©xico ','CMX');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (8,'Coahuila','COA');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (9,'Colima','COL');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (10,'Durango','DUR');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (11,'Guanajuato','GUA');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (12,'Guerrero','GRO');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (13,'Hidalgo','HID');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (14,'Jalisco','JAL');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (15,'MÃ©xico','MEX');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (16,'MichoacÃ¡n','MIC');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (17,'Morelos','MOR');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (18,'Nayarit','NAY');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (19,'Nuevo LeÃ³n','NLE');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (20,'Oaxaca ','OAX');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (21,'Puebla ','PUE');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (22,'QuerÃ©taro ','QUE');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (23,'Quintana Roo ','ROO');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (24,'San Luis PotosÃ­ ','SLP');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (25,'Sinaloa ','SIN');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (26,'Sonora ','SON');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (27,'Tabasco ','TAB');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (28,'Tamaulipas ','TAM');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (29,'Tlaxcala ','TLA');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (30,'Veracruz ','VER');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (31,'YucatÃ¡n ','YUC');
+Insert into ALEJANDRO.ESTADO (ID,ESTADO,ABREVIACION) values (32,'Zacatecas ','ZAC');
